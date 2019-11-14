2937e49728205696b73d3a531de14552ec19e44d
ksg <refresh6724@soongsil.ac.kr>
2019-11-14 11:12:06 +0900

    20191114

--- src/backjoon/Main_15686_치킨배달.java
+++ src/backjoon/Main_15686_치킨배달.java
@@ -1,5 +1,4 @@
 package backjoon;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -7,12 +6,8 @@ import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class Main_15686_치킨배달 {
-	public static int n, m, city[][];
-	public static ArrayList<int[]> chicken = new ArrayList<>();
-	public static ArrayList<int[]> select = new ArrayList<>();
-	public static ArrayList<int[]> house = new ArrayList<>();
-	public static boolean visited[];
-	public static int res = -1;
+	public static int n, m, numbers[], totalHouse, totalChicken, ans = Integer.MAX_VALUE;
+	public static ArrayList<int[]> house, chicken;
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -21,50 +16,50 @@ public class Main_15686_치킨배달 {
 		n = Integer.parseInt(st.nextToken());
 		m = Integer.parseInt(st.nextToken());
 		
-		city = new int[n][n];
+		numbers = new int[m];
+		
+		house = new ArrayList<>();
+		chicken = new ArrayList<>();
 		
 		for(int i=0; i<n; i++) {
 			st = new StringTokenizer(br.readLine(), " ");
 			for(int j=0; j<n; j++) {
-				city[i][j] = Integer.parseInt(st.nextToken());
-				if(city[i][j] == 1) house.add(new int[] {i, j});
-				else if(city[i][j] == 2) chicken.add(new int[] {i, j});
+				int tmp = Integer.parseInt(st.nextToken());
+				if(tmp == 1) house.add(new int[] {i, j});
+				else if(tmp == 2) chicken.add(new int[] {i, j});
 			}
 		}
 		
-		visited = new boolean[chicken.size()];
+		totalHouse = house.size();
+		totalChicken = chicken.size();
 		
 		dfs(0, 0);
 		
-		System.out.println(res);
+		System.out.println(ans);
 	}
 	
-	public static void dfs(int start, int depth) {
+	public static void dfs(int depth, int start) {
 		if(depth == m) {
 			int sum = 0;
 			
-			for(int i=0; i<house.size(); i++) {
+			for(int i=0; i<totalHouse; i++) {
+				int min = Integer.MAX_VALUE;
 				int[] h = house.get(i);
-				int min = -1;
-				for(int j=0; j<select.size(); j++) {
-					if(min == -1) min = Math.abs(select.get(j)[0] - h[0]) + Math.abs(select.get(j)[1] - h[1]);
-					else min = Math.min(min, Math.abs(select.get(j)[0] - h[0]) + Math.abs(select.get(j)[1] - h[1]));
+				
+				for(int j=0; j<m; j++) {
+					int[] c = chicken.get(numbers[j]);
+					
+					min = Math.min(min, Math.abs(c[0] - h[0]) + Math.abs(c[1] - h[1]));
 				}
 				sum += min;
 			}
 			
-			if(res == -1) res = sum;
-			else res = Math.min(res, sum);
+			ans = Math.min(ans, sum);
 		}
 		else {
-			for(int i=start; i<chicken.size(); i++) {
-				if(!visited[i]) {
-					visited[i] = true;
-					select.add(chicken.get(i));
-					dfs(i + 1, depth + 1);
-					visited[i] = false;
-					select.remove(select.indexOf(chicken.get(i)));
-				}
+			for(int i=start; i<totalChicken; i++) {
+				numbers[depth] = i;
+				dfs(depth + 1, i + 1);
 			}
 		}
 	}
