9430e58c88a175ccf3602dbb77669747575fd67a
KMG0908 <sdm6067@naver.com>
2019-12-28 15:00:59 +0900

    20191228

--- src/backjoon/Main_1194_달이차오른다가자.java
+++ src/backjoon/Main_1194_달이차오른다가자.java
@@ -1,7 +1,10 @@
 package backjoon;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class Main_1194_달이차오른다가자 {
 	public static void main(String[] args) {
-		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	}
 }
