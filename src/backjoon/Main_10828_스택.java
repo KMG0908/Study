cad7e6eb96b439fee54e3b8352eb71c92d0c4c28
KMG0908 <sdm6067@naver.com>
2020-02-08 16:10:52 +0900

    20200208

--- src/backjoon/Main_10828_스택.java
+++ src/backjoon/Main_10828_스택.java
@@ -10,11 +10,11 @@ public class Main_10828_스택 {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		int n = Integer.parseInt(br.readLine());
+		int N = Integer.parseInt(br.readLine());
 		Stack<Integer> stack = new Stack<>();
 		StringBuffer sb = new StringBuffer();
 		
-		for(int i=0; i<n; i++){
+		for(int i=0; i<N; i++){
 			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 			
 			switch(st.nextToken()){
