0683667b4893056c8af65d6e7c0927d39c07c7f1
KMG0908 <sdm6067@naver.com>
2020-02-11 23:09:36 +0900

    20200211

---
+++ src/backjoon/Main_2443_별찍기6.java
@@ -0,0 +1,27 @@
+package backjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main_2443_별찍기6 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		StringBuffer sb = new StringBuffer();
+		
+		for(int i=0; i<n; i++){
+			for(int j=0; j<i; j++){
+				sb.append(" ");
+			}
+			for(int j=i; j<n*2-i-1; j++){
+				sb.append("*");
+			}
+			sb.append("\n");
+		}
+		
+		System.out.println(sb.toString());
+	}
+}
