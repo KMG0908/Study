d8e13f3777c48dca053b6276871854e3cba0925d
김민경 <sdm6067@naver.com>
2020-04-02 16:29:55 +0900

    20200402

---
+++ src/backjoon/Main_10817_세수.java
@@ -0,0 +1,24 @@
+package backjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main_10817_세수 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int[] num = new int[3];
+		
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		for(int i=0; i<3; i++) {
+			num[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(num);
+		
+		System.out.println(num[1]);
+	}
+}
