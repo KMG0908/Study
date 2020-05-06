690eb8f2f865802d83068393dd35e737ebfa3250
김민경 <sdm6067@naver.com>
2020-05-06 18:08:36 +0900

    20200506

---
+++ src/backjoon/Main_10870_피보나치수5.java
@@ -0,0 +1,21 @@
+package backjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main_10870_피보나치수5 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		System.out.println(fibonacci(n));
+	}
+	
+	public static int fibonacci(int n) {
+		if(n <= 1) return n;
+		
+		return fibonacci(n - 1) + fibonacci(n - 2);
+	}
+}
