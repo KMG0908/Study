db2257f24a7a225590dbf25a869f3a13485da021
KMG <sdm6067@naver.com>
2020-09-09 19:57:37 +0900

    20200909

--- 알고리즘/src/algorithm/DFS_순열_오름차순.java
+++ 알고리즘/src/algorithm/DFS_순열_오름차순.java
@@ -5,9 +5,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class DFS_순열 {
+public class DFS_순열_오름차순 {
 	public static int n, r, numbers[];
-	public static boolean visited[];
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -17,25 +16,20 @@ public class DFS_순열 {
 		r = Integer.parseInt(st.nextToken());
 		
 		numbers = new int[r];
-		visited = new boolean[n];
 		
-		dfs(0);
+		dfs(0, 0);
 	}
 	
-	public static void dfs(int depth) {
+	public static void dfs(int start, int depth) {
 		if(depth == r) {
 			for(int i=0; i<r; i++) System.out.print(numbers[i] + " ");
 			System.out.println();
 			return;
 		}
 		
-		for(int i=0; i<n; i++) {
-			if(visited[i]) continue;
-			
+		for(int i=start; i<n; i++) {
 			numbers[depth] = i + 1;
-			visited[i] = true;
-			dfs(depth + 1);
-			visited[i] = false;
+			dfs(i + 1, depth + 1);
 		}
 	}
 }
