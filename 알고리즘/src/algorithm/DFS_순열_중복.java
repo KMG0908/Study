e0af5c272971b5bee2dd3eaf7bcbab11763b90d1
KMG <sdm6067@naver.com>
2020-09-12 19:22:33 +0900

    20200912

---
+++ 알고리즘/src/algorithm/DFS_순열_중복.java
@@ -0,0 +1,35 @@
+package algorithm;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class DFS_순열_중복 {
+	public static int n, r, numbers[];
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		
+		n = Integer.parseInt(st.nextToken());
+		r = Integer.parseInt(st.nextToken());
+		
+		numbers = new int[r];
+		
+		dfs(0);
+	}
+	
+	public static void dfs(int depth) {
+		if(depth == r) {
+			for(int i=0; i<r; i++) System.out.print(numbers[i] + " ");
+			System.out.println();
+			return;
+		}
+		
+		for(int i=0; i<n; i++) {
+			numbers[depth] = i + 1;
+			dfs(depth + 1);
+		}
+	}
+}
